d60b72f9f6db48a4d20068e2f65ab5ec782e6eb2
Loppone <shaolinlohan@gmail.com>
2023-02-23 00:12:43 +0100

    Modifica client

--- FFQueryBuilderClient/Program.cs
+++ FFQueryBuilderClient/Program.cs
@@ -3,6 +3,7 @@ using FFQueryBuilderClient.Models;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
+using System;
 
 namespace FFQueryBuilderClient
 {
@@ -51,8 +52,6 @@ namespace FFQueryBuilderClient
                 var qs = q.ToQueryString();
 
                 var res = q.ToList();
-
-                FFQueryBuilder.IFilterValue
             }
         }
     }
